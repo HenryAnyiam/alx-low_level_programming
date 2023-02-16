2e19163c468bdf79ee25e88c59d86dbc568d90c3
HenryAnyiam <anyiamhenry7@gmail.com>
2023-02-16 06:24:24 -0800

    New

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  *main: prints string using printf
+  *Return: 0 to end function
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
