e7565bb9fddcd5dd209dd09057266b986cc65f18
HenryAnyiam <anyiamhenry7@gmail.com>
2023-02-17 03:13:32 -0800

    First corrections

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
   *main- prints if n is positive or negative
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
   *main- if last digit of an integer is positive
@@ -15,9 +16,6 @@ int main(void)
 	c = n % 10;
 	if (c > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n,c);
-	else if (c == 0)
-		printf("Last digit of %d is %d and is 0\n", n,c);
-	else if (c < 6 && c > 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,c);
+	else if (c
 	return (0);
 }
