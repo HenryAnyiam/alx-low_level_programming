46c4d1d3ff8bfba81f021484e9a4da0a54195be5
HenryAnyiam <anyiamhenry7@gmail.com>
2023-02-21 15:06:18 -0800

    Created _putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
   *main - prints _putchar
@@ -6,6 +6,23 @@
   */
 int main(void)
 {
-	printf("_putchar\n");
+	int n[9];
+	int i;
+
+	n[0] = '_';
+	n[1] = 'p';
+	n[2] = 'u';
+	n[3] = 't';
+	n[4] = 'c';
+	n[5] = 'h';
+	n[6] = 'a';
+	n[7] = 'r';
+	n[8] = '\n';
+	i = 0;
+	while (i <= 8)
+	{
+		_putchar(n);
+		++i;
+	}
 	return (0);
 }
