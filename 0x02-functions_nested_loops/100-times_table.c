ae1524c778b7862f69d37d9d5e204bd492841021
HenryAnyiam <anyiamhenry7@gmail.com>
2023-02-21 20:34:04 -0800

    New function

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+  *print_times_table - prints times table
+  *@n: integer
+  */
+void print_times_table(int n)
+{
+	int a, s, n, c, i, m, g, h, j;
+
+	a = 0;
+	s = ' ';
+	n = '\n';
+	c = ',';
+	if (!((n > 15) || (n < 0)))
+	{
+		while (a <= n)
+		{
+			i = 0;
+			m = 0;
+			while (i <= n)
+			{
+				if ((m == 0) && (i == 0))
+					_putchar(m + '0');
+				else if (m <= 9)
+				{
+					_putchar(s);
+					_putchar(m + '0');
+				}
+				else
+				{
+					if (m <= 99)
+					{
+						g = m / 10;
+						h = m % 10;
+						_putchar(g + '0');
+						_putchar(h + '0');
+					}
+					else
+					{
+						g = m / 100;
+						h = m % 100;
+						j = (m /10) % 10;
+						_putchar(g + '0');
+						_putchar(j + '0');
+						_putchar(h + '0');
+					}
+				}
+				if (i != n)
+				{
+					_putchar(c);
+					_putchar(s);
+				}
+				m = m + a;
+			++i;
+			}
+			_putchar(n);
+			++a;
+		}
+	}
+}
