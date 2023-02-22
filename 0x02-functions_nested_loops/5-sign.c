a0b6fc042e8889b7fee6494eaeee8908909901bf
HenryAnyiam <anyiamhenry7@gmail.com>
2023-02-21 16:08:52 -0800

    Nw function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  *print_sign - prints the sign of a number
+  *@n: number to check 
+  (*
+  *Return: sign of number
+  */
+int print_sign(int n)
+{
+	int p;
+	int m;
+	int val;
+
+	val = 0;
+	p = '+';
+	m = '-';
+	if (n > 0)
+	{
+		_putchar(p);
+		val = 1;
+	}
+	else if (n < 0)
+	{
+		_putchar(m);
+		val = "-1"
+	}
+	else
+		_putchar(val);
+	return (val);
+}
