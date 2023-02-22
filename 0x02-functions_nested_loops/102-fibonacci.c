1c7de36b9e9cf0c00bc6946f7ad364a67e3ef558
HenryAnyiam <anyiamhenry7@gmail.com>
2023-02-21 21:00:31 -0800

    new

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  *main - prints fibonacci sequence
+  *Return: 0 to end main
+  */
+int main(void)
+{
+	int a;
+	int b;
+	int i;
+	int sum;
+
+	i = 0;
+	a = 1;
+	b = 2;
+	sum = 0;
+	while (i <= 50)
+	{
+		printf("%d,%d,", a, b);
+		sum = b;
+		b = a + b;
+		a = b;
+	}
+	printf("\n");
+}
