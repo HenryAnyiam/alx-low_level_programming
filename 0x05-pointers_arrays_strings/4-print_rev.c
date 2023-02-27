8559b703a047f9444ca441eeaf1aeaab80577042
HenryAnyiam <anyiamhenry7@gmail.com>
2023-02-27 11:42:38 -0800

    new

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *print_rev - prints string in reverse
+  *@s: string to print
+  */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		++i;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		--l;
+	}
+	_putchar('\n');
+}
