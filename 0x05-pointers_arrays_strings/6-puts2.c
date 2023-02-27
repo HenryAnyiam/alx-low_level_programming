b3cc09b00c05ee09578764914e9490466138c958
HenryAnyiam <anyiamhenry7@gmail.com>
2023-02-27 14:21:51 -0800

    correcrion

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,10 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (*(s + i) != '\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(*(s + i));
+		_putchar(*(str + i));
 		i = i + 2;
 	}
-	_putchat('\n');
+	_putchar('\n');
 }
