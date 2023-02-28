257ec936c09077f564bf327c5ae093e097c6e824
HenryAnyiam <anyiamhenry7@gmail.com>
2023-02-27 17:27:30 -0800

    new function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  *_atoi - converts a string to an integer
+  *@s: string to be used
+  (*
+  *Return: ineger to be returned
+  */
+int _atoi(char *s)
+{
+	int i, j;
+	unsigned int a;
+
+	i =  0;
+	j = 1;
+	a = 0;
+	while ((*(s + i) != '\0') && (!(s[i] >= '0' && s[i] <= '9')))
+	{
+		if (s[i] == '-')
+			j = -(1);
+
+		++i;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		a = (a * 10) + (s[i] - '0');
+		++i;
+	}
+	a = a * j;
+	return (a);
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,7 +15,7 @@ char *_strcpy(char *dest, char *src)
 	while (*(src + i) != '\0')
 		++i;
 	j = 0;
-	while(j <= i)
+	while (j <= i)
 	{
 		dest[j] = src[j];
 		++j;
