8cd3278d608b68ce5d0b4b2b84e165a244ff4146
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-02 15:04:33 -0800

    New Array Function

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  *cap_string - capitalizes all words of a string
+  *@a: string
+  *)
+  *Return: capitalized string
+  */
+char *cap_string(char *a)
+{
+	int i;
+	char l, u;
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		if (a[i] == ' ' || a[i] == '\n' || a[i] == '\t' || a[i] == ','
+				|| a[i] == ';' || a[i] == ':' || a[i] == '!' || a[i] == '?'
+				|| a[i] == '"' || a[i] == '(' || a[i] == ')' || a[i] == '{'
+				|| a[i] == '}' || a[i] >= '0' &a[i] <= '9')
+		{
+			l = 'a';
+			u = 'A';
+			++i;
+			while (l <= 'z')
+			{
+				if (a[i] == l)
+					a[i] = u;
+				++u;
+				++l;
+			}
+			--i;
+		}
+		++i;
+	}
+	return (a);
+}
