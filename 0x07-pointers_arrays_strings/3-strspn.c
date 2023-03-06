221e11a7bb43b837d5b64b3761acd4eff96deaee
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-06 09:31:13 -0800

    New Array Function

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  *_strnspn - gets the length of a prefix substring.
+  *@s: string to be checked
+  *@accept: string to check with
+  *)
+  *Return: number of bytes
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a;
+	char *swap;
+
+	swap = accept;
+	a = 0;
+	while (*s++)
+	{
+		while (*accept++)
+		{
+			if (*(s - 1) == *(accept - 1))
+			{
+				++a;
+				break;
+			}
+		}
+		if (!(*--accept))
+			break;
+		accept = swap;
+	}
+	return (a);
+}
