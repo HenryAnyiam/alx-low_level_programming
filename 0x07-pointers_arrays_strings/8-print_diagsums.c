adfac9ed852ae90a79c2b259fa794531859cc867
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-06 14:05:32 -0800

    New Pointer Function

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-  *print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+  *print_diagsums - prints the sum of the two diagonals of a square matrix
   *@a: pointer to array
   *@size: size of array
   */
