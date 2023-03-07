2d740be202eab1940881000dda74c3dfce006e6a
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-07 07:46:47 -0800

    Recursive function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  *_pow_recursion - finds the value of x to power y
+  *@x: integer
+  *@y: power
+  *)
+  *Return: final result
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	y--;
+	return (x * _pow_recursion(x, y));
+}
