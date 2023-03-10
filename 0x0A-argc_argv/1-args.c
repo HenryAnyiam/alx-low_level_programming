9d5019332ca96eac12e928b1811b4c158ce3ce47
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-10 05:26:44 -0800

    New argc implementations

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *main - prints outs number of arguments
+  *@argc: number of arguments
+  *@argv: array of arguments
+  *)
+  *Return: 0
+  */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
