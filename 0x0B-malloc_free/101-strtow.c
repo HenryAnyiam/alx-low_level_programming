e8bd67ce2e6d161b65c990f7756d8aa7285645ad
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-14 15:37:03 -0700

    Malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,7 +13,7 @@ char **strtow(char *str)
 	char **s;
 	unsigned int i, j, c;
 
-	if (str == NULL || str[0] == '\0')
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 		return (NULL);
 	c = 0;
 	for (i = 0; str[i] != '\0'; i++)
@@ -21,45 +21,29 @@ char **strtow(char *str)
 			c++;
 	s = malloc(sizeof(char *) * (c + 1));
 	if (s == NULL)
-	{
-		free(s);
 		return (NULL);
-	}
-	c = 0;
-	j = 0;
+	c = j = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ' && str[i + 1] != ' ')
+		if (str[i] != ' ')
 			++j;
-		else if ((str[i] != ' ') && ((str[i + 1] == ' ') || (str[i + 1] == '\0')))
+		if ((str[i] != ' ') && ((str[i + 1] == ' ') || (str[i + 1] == '\0')))
 		{
 			s[c] = malloc(sizeof(char) * (j + 1));
 			j = 0;
 			if (s[c] == NULL)
-			{
-				for (; c > 0; c--)
-					free(s[c]);
-				free(s[c]);
-				free(s);
 				return (NULL);
-			}
 			++c;
 		}
 	}
-	c = 0;
-	j = 0;
+	c = j = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] != ' ') && (str[i + 1] != ' '))
+		if (str[i] != ' ')
+			s[c][j++] = str[i];
+		if ((str[i] != ' ') && ((str[i + 1] == ' ') || str[i + 1] == '\0'))
 		{
-			s[c][j] = str[i];
-			++j;
-		}
-		else if ((str[i] != ' ') && ((str[i + 1] == ' ') || str[i + 1] == '\0'))
-		{
-			s[c][j] = str[i];
-			s[c][j + 1] = '\0';
-			++c;
+			s[c++][j] = '\0';
 			j = 0;
 		}
 	}
