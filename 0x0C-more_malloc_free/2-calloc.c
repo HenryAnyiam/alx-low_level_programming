e976bb753c212f0fe094b112cda6606210701d9c
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-16 13:58:58 -0700

    Malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  *_calloc - allocates memory and initialize to 0
+  *@nmemb: number of elements
+  *@size: bytes
+  *)
+  *Return: pointer or null
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i <= (nmemb * size); i++)
+		*((char *)p + 1) = 0;
+	return (p);
+}
