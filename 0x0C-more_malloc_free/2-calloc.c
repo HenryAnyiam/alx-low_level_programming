29c3784b278ef213b344460fc3303ccf30907049
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-16 14:04:38 -0700

    Malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,6 +19,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
-		*((char *)p + 1) = 0;
+		*((char *)p + i) = 0;
 	return (p);
 }
