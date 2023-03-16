1ec2383a47412cf46ae867e9ee93a5c1863ba615
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-16 15:08:42 -0700

    Malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	hold = malloc(old_size);
 	if (hold != NULL)
-		for (i = 0; i < old_size; i++)
+		for (i = 0; i <= old_size; i++)
 			*((char *)(hold) + i) = *((char *)(ptr) + i);
 	free(ptr);
 	ptr = malloc(new_size);
@@ -37,7 +37,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		a = new_size;
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < a; i++)
+	for (i = 0; i <= a; i++)
 		*((char *)(ptr) + i) = *((char *)(hold) + i);
 	free(hold);
 	return (ptr);
