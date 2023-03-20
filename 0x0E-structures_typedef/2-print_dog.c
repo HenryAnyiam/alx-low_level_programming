4d3d813c98321161eb6e10c892b8618a59d311db
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-20 16:55:41 -0700

    Corrected stdout fault

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
-			printf("Age: 0\n");
+			printf("Age: %f\n", 0);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
