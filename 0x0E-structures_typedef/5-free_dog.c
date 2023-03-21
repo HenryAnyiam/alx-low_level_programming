5540a08c19ea43f07f291f4a6859b74e632288aa
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-20 18:03:22 -0700

    Corrected compile error

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+  *free_dog - free dog
+  *@d: pointer to structure
+  */
 void free_dog(dog_t *d)
 {
 	free(d->name);
-	free(d->age);
 	free(d->owner);
 	free(d);
 }
