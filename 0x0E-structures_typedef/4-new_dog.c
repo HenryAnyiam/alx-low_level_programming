4e4a0b98ae0e6b709186096498cb6448162f9e31
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-20 17:38:37 -0700

    New structure program

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,38 @@
 #include "dog.h"
 
 /**
+  *_strlen - counts length of string
+  *@str: string
+  *)
+  *Return: length
+  */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+  *_strcpy - copies a string
+  *@a: string to copy from
+  *@b: string to copy to
+  *)
+  *Return: copied string
+  */
+void _strcpy(char *a, char *b)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+		b[i] = a[i];
+	b[i] = '\0';
+	return (b);
+}
+
+/**
   *new_dog - creates a new dog
   *@name: Name
   *@age: Age
@@ -15,9 +47,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
-		return (new);
-	new->name = name;
+		return (NULL);
+	new->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	new->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	if (new->owner == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->name = _strcpy(name, new->name);
 	new->age = age;
-	new->owner = owner;
+	new->owner = _strcpy(owner, new->owner);
 	return (new);
 }
