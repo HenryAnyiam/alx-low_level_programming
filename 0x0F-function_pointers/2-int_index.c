5614ae43b73954d72654ede37c06776282be3a93
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-22 04:42:56 -0700

    Debugging functions

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (cmp != NULL)
+	if ((cmp != NULL) && (array[size - 1] != NULL))
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]) != 0)
 				return (i);
