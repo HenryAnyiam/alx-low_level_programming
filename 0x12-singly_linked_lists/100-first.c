1216b2dd5142306e8a8b77c2ea6faee135613764
HenryAnyiam <anyiamhenry7@gmail.com>
2023-03-30 17:16:58 -0700

    Advanced functions

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void first_print(void) __attribute__ ((constructor));
+/**
+  *first_print - prints before main function is executed
+  */
+void first_print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
