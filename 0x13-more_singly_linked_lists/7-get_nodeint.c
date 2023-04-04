e56d5dea0963ae811ba7e53bce91347d992cf7f2
HenryAnyiam <anyiamhenry7@gmail.com>
2023-04-04 01:32:52 -0700

    Corrected compile error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *hold = head;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 	while (hold)
 	{
