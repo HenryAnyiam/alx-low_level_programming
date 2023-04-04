a1d8b01a5dd6a865c8020f942fce16a69fafc9af
HenryAnyiam <anyiamhenry7@gmail.com>
2023-04-04 10:14:27 -0700

    Free linked list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  *free_listint_safe - frees alinked list
+  *@h: pointer to first node
+  *)
+  *Return: size of list
+  */
+size_t free_listint_safe(listint_t **h)
+{
+	long int pt;
+	size_t i = 0;
+	listint_t *temp;
+
+	if (h == NULL)
+		return (0);
+	while (*h)
+	{
+		i++;
+		pt = (*h) - (*h)->next;
+		if (pt > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			(*h) = temp;
+		}
+		else
+		{
+			(*h) = NULL;
+			break;
+		}
+	}
+	return (i);
+}
