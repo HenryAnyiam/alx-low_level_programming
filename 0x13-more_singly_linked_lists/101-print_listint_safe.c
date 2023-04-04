6f092c4e6090bbb632d16a0684461a068867e683
HenryAnyiam <anyiamhenry7@gmail.com>
2023-04-04 10:52:17 -0700

    Tried making changes to saf print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,20 +13,18 @@ size_t print_listint_safe(const listint_t *head)
 	long int pt;
 	size_t i = 0;
 
-	if (head == NULL)
-		exit(98);
 	while (head)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
-		pt = head - head->next;
 		i++;
-		if (pt < 0)
+		pt = head - head->next;
+		if (pt > 0)
+			head = head->next;
+		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
-		else
-			head = head->next;
 	}
 	return (i);
 }
