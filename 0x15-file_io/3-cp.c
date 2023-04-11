aa1e839dfe0ecb6fe27cec34301566a6e6f942b1
HenryAnyiam <anyiamhenry7@gmail.com>
2023-04-10 17:32:56 -0700

    Corrected betty style error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,51 @@
 #include "main.h"
 
 /**
+  *check_arg - checks number of arguments
+  *@arg: number of arguments
+  */
+void check_arg(int arg)
+{
+	if (arg != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+
+/**
+  *close_file - check if files closed successfully
+  *@fd: file descriptor
+  */
+void close_file(int fd)
+{
+	int i = close(fd);
+
+	if (i < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+  *check_read - check successful file read
+  *@r: read return
+  *@ff: file from descriptor
+  *@ft: file to descriptor
+  *@file: file name
+  */
+void check_read(int r, int ff, int ft, char *file)
+{
+	if (r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		close_file(ff);
+		close_file(ft);
+		exit(98);
+	}
+}
+/**
   *main - copy from one file to the other
   *@argc: number of given arguments
   *@argv: array of given arguments
@@ -13,14 +58,10 @@
   */
 int main(int argc, char **argv)
 {
-	int ft, ff, r_check, w_check;
+	int ft, ff, r_check;
 	char buf[1024];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	check_arg(argc);
 	ff = open(argv[1], O_RDONLY);
 	ft = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (ff < 0)
@@ -33,37 +74,18 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	r_check = 1;
-	while (r_check > 0)
+	while ((r_check = read(ff, buf, 1024)) > 0)
 	{
-		r_check = read(ff, buf, 1024);
-		w_check = write(ft, buf, r_check);
-		if ((w_check < 0) || (w_check != r_check))
+		if ((write(ft, buf, r_check)) != r_check)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(ff);
-			close(ft);
+			close_file(ff);
+			close_file(ft);
 			exit(99);
 		}
 	}
-	if (r_check < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(ff);
-		close(ft);
-		exit(98);
-	}
-	r_check = close(ff);
-	w_check = close(ft);
-	if (r_check < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff);
-		exit(100);
-	}
-	if (w_check < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft);
-		exit(100);
-	}
+	check_read(r_check, ff, ft, argv[1]);
+	close_file(ff);
+	close_file(ft);
 	return (0);
 }
