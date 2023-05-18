5922e0b70a87a2a9389c541fd00090909b213f80
HenryAnyiam <anyiamhenry7@gmail.com>
2023-05-18 02:15:22 -0700

    prints DLL elements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t i = 0;
+	
+	if (h == NULL)
+		return (i);
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
