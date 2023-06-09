0b372f45f6c024eb74bf462d633cdec9ea28f7a3
HenryAnyiam <anyiamhenry7@gmail.com>
2023-06-08 17:33:27 -0700

    insert in table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *data, *curr;
 
 	if (strlen(key) == 0)
-		return (0);
+		return (1);
 	index = key_index((const unsigned char *)key, ht->size);
 	data = malloc(sizeof(hash_node_t));
 	if (data == NULL)
