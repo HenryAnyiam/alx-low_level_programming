c578ade4f2e4f8cb9cd5a0231da140c505410f6e
HenryAnyiam <anyiamhenry7@gmail.com>
2023-06-22 15:19:49 -0700

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	long i, max, num = 612852475143;
+	double sqr = sqrt(num);
+
+	for (i = 0; i <= sqr; i++)
+	{
+		if (num % i == 0)
+			max = num / i;
+	}
+	printf("%ld\n", max);
+	return (0);
+}
