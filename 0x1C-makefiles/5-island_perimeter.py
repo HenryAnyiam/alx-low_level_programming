0afa346d02fad77109d90529258dc8b76fc9c20c
HenryAnyiam <anyiamhenry7@gmail.com>
2023-07-26 19:39:40 -0700

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+module witg function -island_perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    calculates the permeter of an island
+
+    Args:
+        grid: list of list
+
+    """
+
+    perimeter = 0
+    for i in grid:
+        for j in i:
+            if j == 1:
+                perimeter += 2
+    if perimeter > 0:
+        perimeter += 2
+    return perimeter
