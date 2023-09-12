c77b07c1a30f086fef253ceebb275df9638ee179
HenryAnyiam <anyiamhenry7@gmail.com>
2023-09-12 03:45:57 -0700

    added space and time complexity

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,7 +15,7 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	while (head < tail)
+	while (head <= tail)
 	{
 		printf("Searching in array: ");
 		for (i = head; i < tail; i++)
